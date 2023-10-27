6d21046099729c3c4e7081caf9b5fd42cf706ad4
BGluth <gluthb@gmail.com>
2023-10-26 19:46:11 -0600

    Boilerplate for compact nodes

--- src/compact/compact_to_partial_trie.rs
+++ src/compact/compact_to_partial_trie.rs
@@ -20,18 +20,82 @@ pub(super) fn create_partial_trie_from_remaining_witness_elem(
 }
 
 pub(super) fn create_partial_trie_from_remaining_witness_elem_rec(
-    _curr_key: Nibbles,
+    curr_key: Nibbles,
     curr_node: &NodeEntry,
-    _p_trie: &mut HashedPartialTrie,
+    p_trie: &mut HashedPartialTrie,
 ) -> CompactParsingResult<()> {
     match curr_node {
-        NodeEntry::Account(_) => todo!(),
-        NodeEntry::Branch(_) => todo!(),
-        NodeEntry::Code(_) => todo!(),
-        NodeEntry::Empty => todo!(),
-        NodeEntry::Hash(_) => todo!(),
-        NodeEntry::Leaf(_, _) => todo!(),
-        NodeEntry::Extension(_, _) => todo!(),
-        NodeEntry::Value(_) => todo!(),
+        NodeEntry::Account(_) => process_account(curr_key, curr_node, p_trie),
+        NodeEntry::Branch(_) => process_branch(curr_key, curr_node, p_trie),
+        NodeEntry::Code(_) => process_code(curr_key, curr_node, p_trie),
+        NodeEntry::Empty => process_empty(curr_key, curr_node, p_trie),
+        NodeEntry::Hash(_) => process_hash(curr_key, curr_node, p_trie),
+        NodeEntry::Leaf(_, _) => process_leaf(curr_key, curr_node, p_trie),
+        NodeEntry::Extension(_, _) => process_extension(curr_key, curr_node, p_trie),
+        NodeEntry::Value(_) => process_value(curr_key, curr_node, p_trie),
     }
+}
+
+fn process_account(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_branch(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_code(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_empty(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_hash(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_leaf(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_extension(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
+}
+
+fn process_value(
+    _curr_key: Nibbles,
+    _curr_node: &NodeEntry,
+    _p_trie: &mut HashedPartialTrie,
+) -> CompactParsingResult<()> {
+    todo!()
 }
