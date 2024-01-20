cf1dffd6f761879fc3a0f297efccd560fab431eb
Robin Salen <salenrobin@gmail.com>
2024-01-19 19:10:02 -0500

    Allow verifier to load only necessary data

--- zero_bin/verifier/src/cli.rs
+++ zero_bin/verifier/src/cli.rs
@@ -1,10 +1,15 @@
 use std::path::PathBuf;
 
 use clap::{Parser, ValueHint};
+use common::prover_state::cli::CliProverStateConfig;
 
 #[derive(Parser)]
 pub(crate) struct Cli {
     /// The file containing the proof to verify
     #[arg(short, long, value_hint = ValueHint::FilePath)]
     pub(crate) file_path: PathBuf,
+    /// The prover configuration used to generate the preprocessed circuits
+    /// and the verifier state.
+    #[clap(flatten)]
+    pub(crate) prover_state_config: CliProverStateConfig,
 }
