b994373faa4284660ffac18f517bd941968a677f
Ayush Shukla <shuklaayush247@gmail.com>
2024-02-19 13:35:33 +0100

    fix(evm_arithmetization): fix padding constraint in keccak_stark (#48)
    * fix: remove duplicate constraint
    
    * fix: add proper constraints for padding rows
    
    * fix: remove extra constraints
    
    * fix: simplify constraints using mul_sub gate
    
    * fix: move constraint outside loop
    
    * chore: rename
    
    * chore: add comment explaining constrain
    
    * fix: remove extra sub constraint

--- evm_arithmetization/src/keccak/round_flags.rs
+++ evm_arithmetization/src/keccak/round_flags.rs
@@ -25,17 +25,23 @@ pub(crate) fn eval_round_flags<F: Field, P: PackedField<Scalar = F>>(
     }
 
     // Flags should circularly increment, or be all zero for padding rows.
-    let next_any_flag = (0..NUM_ROUNDS).map(|i| next_values[reg_step(i)]).sum::<P>();
-    for i in 0..NUM_ROUNDS {
-        let current_round_flag = local_values[reg_step(i)];
-        let next_round_flag = next_values[reg_step((i + 1) % NUM_ROUNDS)];
-        yield_constr.constraint_transition(next_any_flag * (next_round_flag - current_round_flag));
-    }
-
-    // Padding rows should always be followed by padding rows.
     let current_any_flag = (0..NUM_ROUNDS)
         .map(|i| local_values[reg_step(i)])
         .sum::<P>();
+    let next_any_flag = (0..NUM_ROUNDS).map(|i| next_values[reg_step(i)]).sum::<P>();
+    // Padding row should only start after the last round row.
+    let last_round_flag = local_values[reg_step(NUM_ROUNDS - 1)];
+    let padding_constraint =
+        (next_any_flag - F::ONE) * current_any_flag * (last_round_flag - F::ONE);
+    for i in 0..NUM_ROUNDS {
+        let current_round_flag = local_values[reg_step(i)];
+        let next_round_flag = next_values[reg_step((i + 1) % NUM_ROUNDS)];
+        yield_constr.constraint_transition(
+            next_any_flag * (next_round_flag - current_round_flag) + padding_constraint,
+        );
+    }
+
+    // Padding rows should always be followed by padding rows.
     yield_constr.constraint_transition(next_any_flag * (current_any_flag - F::ONE));
 }
 
@@ -56,19 +62,25 @@ pub(crate) fn eval_round_flags_recursively<F: RichField + Extendable<D>, const D
     }
 
     // Flags should circularly increment, or be all zero for padding rows.
+    let current_any_flag =
+        builder.add_many_extension((0..NUM_ROUNDS).map(|i| local_values[reg_step(i)]));
     let next_any_flag =
         builder.add_many_extension((0..NUM_ROUNDS).map(|i| next_values[reg_step(i)]));
+    // Padding row should only start after the last round row.
+    let last_round_flag = local_values[reg_step(NUM_ROUNDS - 1)];
+    let padding_constraint = {
+        let tmp = builder.mul_sub_extension(current_any_flag, next_any_flag, current_any_flag);
+        builder.mul_sub_extension(tmp, last_round_flag, tmp)
+    };
     for i in 0..NUM_ROUNDS {
         let current_round_flag = local_values[reg_step(i)];
         let next_round_flag = next_values[reg_step((i + 1) % NUM_ROUNDS)];
-        let diff = builder.sub_extension(next_round_flag, current_round_flag);
-        let constraint = builder.mul_extension(next_any_flag, diff);
+        let flag_diff = builder.sub_extension(next_round_flag, current_round_flag);
+        let constraint = builder.mul_add_extension(next_any_flag, flag_diff, padding_constraint);
         yield_constr.constraint_transition(builder, constraint);
     }
 
     // Padding rows should always be followed by padding rows.
-    let current_any_flag =
-        builder.add_many_extension((0..NUM_ROUNDS).map(|i| local_values[reg_step(i)]));
     let constraint = builder.mul_sub_extension(next_any_flag, current_any_flag, next_any_flag);
     yield_constr.constraint_transition(builder, constraint);
 }
