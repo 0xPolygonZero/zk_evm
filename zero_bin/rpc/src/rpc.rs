28356c086b3f74a46eb59c43a7227f5727fbeb28
BGluth <gluthb@gmail.com>
2024-02-21 14:34:28 -0700

    Logic to parse withdrawals from the full node

--- zero_bin/rpc/src/rpc.rs
+++ zero_bin/rpc/src/rpc.rs
@@ -104,6 +104,20 @@ struct EthGetBlockByNumberResult {
     parent_hash: H256,
     state_root: H256,
     timestamp: U256,
+    withdrawals: Vec<Withdrawal>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Withdrawal {
+    address: Address,
+    amount: U256,
+}
+
+impl From<Withdrawal> for (Address, U256) {
+    fn from(v: Withdrawal) -> Self {
+        (v.address, v.amount)
+    }
 }
 
 /// The response from the `eth_getBlockByNumber` RPC method.
@@ -289,6 +303,12 @@ impl From<RpcBlockMetadata> for OtherBlockData {
             block_bloom: bloom,
         };
 
+        let withdrawals = block_by_number
+            .result
+            .withdrawals
+            .into_iter()
+            .map(|w| w.into())
+            .collect();
         Self {
             b_data: BlockLevelData {
                 b_meta: block_metadata,
@@ -296,6 +316,7 @@ impl From<RpcBlockMetadata> for OtherBlockData {
                     prev_hashes,
                     cur_hash: block_by_number.result.hash,
                 },
+                withdrawals,
             },
             checkpoint_state_trie_root,
         }
