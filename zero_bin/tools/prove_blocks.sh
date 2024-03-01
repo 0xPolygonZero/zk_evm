b1020c54750542e8f6851ce8c01affb9e0bf23cf
Robin Salen <salenrobin@gmail.com>
2024-03-01 18:00:26 +0900

    Revert changes on prove_blocks script

--- zero_bin/tools/prove_blocks.sh
+++ zero_bin/tools/prove_blocks.sh
@@ -7,18 +7,18 @@
 # 4 --> Ignore previous proofs (boolean)
 
 export RUST_BACKTRACE=1
-export RUST_LOG=plonky2=info,evm_arithmetization=info
+export RUST_LOG=plonky2=trace,evm_arithmetization=trace
 
-export ARTITHMETIC_CIRCUIT_SIZE="16..17"
-export BYTE_PACKING_CIRCUIT_SIZE="9..10"
-export CPU_CIRCUIT_SIZE="12..13"
-export KECCAK_CIRCUIT_SIZE="14..15"
-export KECCAK_SPONGE_CIRCUIT_SIZE="9..10"
-export LOGIC_CIRCUIT_SIZE="12..13"
-export MEMORY_CIRCUIT_SIZE="17..18"
+export ARTITHMETIC_CIRCUIT_SIZE="16..23"
+export BYTE_PACKING_CIRCUIT_SIZE="9..21"
+export CPU_CIRCUIT_SIZE="12..25"
+export KECCAK_CIRCUIT_SIZE="14..20"
+export KECCAK_SPONGE_CIRCUIT_SIZE="9..15"
+export LOGIC_CIRCUIT_SIZE="12..18"
+export MEMORY_CIRCUIT_SIZE="17..28"
 
 PROOF_OUTPUT_DIR="proofs"
-ALWAYS_WRITE_LOGS=1 # Change this to `1` if you always want logs to be written.
+ALWAYS_WRITE_LOGS=0 # Change this to `1` if you always want logs to be written.
 
 TOT_BLOCKS=$(($2-$1+1))
 IGNORE_PREVIOUS_PROOFS=$4
@@ -44,12 +44,13 @@ do
         fi
     fi
 
-    cargo r --release --features test_only --bin leader -- --runtime in-memory jerigon --rpc-url "$3" --block-number $i --proof-output-path $OUT_PROOF_PATH $PREV_PROOF_EXTRA_ARG > $OUT_LOG_PATH 2>&1
+    cargo r --release --bin leader -- --runtime in-memory jerigon --rpc-url "$3" --block-number $i --proof-output-path $OUT_PROOF_PATH $PREV_PROOF_EXTRA_ARG > $OUT_LOG_PATH 2>&1
     
     retVal=$?
     if [ $retVal -ne 0 ]; then
         # Some error occured.
         echo "Block ${i} errored. See ${OUT_LOG_PATH} for more details."
+        exit $retVal
     else
         # Remove the log on success if we don't want to keep it.
         if [ $ALWAYS_WRITE_LOGS -ne 1 ]; then
