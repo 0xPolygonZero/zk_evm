2c18d4b7951f48cfe9a42f275382ccffa8044062
wborgeaud <williamborgeaud@gmail.com>
2024-04-04 13:24:10 +0200

    Remove ignore and rlp alloc

--- evm_arithmetization/src/cpu/kernel/tests/transaction_parsing/parse_type_0_txn.rs
+++ evm_arithmetization/src/cpu/kernel/tests/transaction_parsing/parse_type_0_txn.rs
@@ -9,7 +9,6 @@ use crate::cpu::kernel::constants::txn_fields::NormalizedTxnField;
 use crate::cpu::kernel::interpreter::Interpreter;
 use crate::memory::segments::Segment;
 
-#[ignore]
 #[test]
 fn process_type_0_txn() -> Result<()> {
     let process_type_0_txn = KERNEL.global_labels["process_type_0_txn"];
