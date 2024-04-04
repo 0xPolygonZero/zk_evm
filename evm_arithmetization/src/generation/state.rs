eb9ea214bf7be778dfb3eb698aeadc572994921e
Robin Salen <salenrobin@gmail.com>
2024-04-04 11:53:54 +0900

    Remove interpreter specific methods from State trait

--- evm_arithmetization/src/generation/state.rs
+++ evm_arithmetization/src/generation/state.rs
@@ -138,12 +138,6 @@ pub(crate) trait State<F: Field> {
     /// Return the offsets at which execution must halt
     fn get_halt_offsets(&self) -> Vec<usize>;
 
-    /// Inserts a preinitialized segment, given as a [Segment],
-    /// into the `preinitialized_segments` memory field.
-    fn insert_preinitialized_segment(&mut self, segment: Segment, values: MemorySegmentState);
-
-    fn is_preinitialized_segment(&self, segment: usize) -> bool;
-
     /// Simulates the CPU. It only generates the traces if the `State` is a
     /// `GenerationState`.
     fn run_cpu(&mut self) -> anyhow::Result<()>
@@ -426,16 +420,6 @@ impl<F: Field> State<F> for GenerationState<F> {
             traces: self.traces.checkpoint(),
             clock: self.get_clock(),
         }
-    }
-
-    fn insert_preinitialized_segment(&mut self, segment: Segment, values: MemorySegmentState) {
-        panic!(
-            "A `GenerationState` cannot have a nonempty `preinitialized_segment` field in memory."
-        )
-    }
-
-    fn is_preinitialized_segment(&self, segment: usize) -> bool {
-        false
     }
 
     fn incr_gas(&mut self, n: u64) {
