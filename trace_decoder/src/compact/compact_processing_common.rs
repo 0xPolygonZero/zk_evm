45b7c22fd6a6bd976fc5e16221b3c4a7747e0b67
BGluth <gluthb@gmail.com>
2024-04-15 17:17:38 -0600

    Added a simple test case

--- trace_decoder/src/compact/compact_processing_common.rs
+++ trace_decoder/src/compact/compact_processing_common.rs
@@ -1033,9 +1033,9 @@ mod tests {
         compact_mpt_processing::process_compact_mpt_prestate_debug,
         compact_processing_common::{key_bytes_to_nibbles, ParserState},
         complex_test_payloads::{
-            TEST_PAYLOAD_1, TEST_PAYLOAD_10, TEST_PAYLOAD_11, TEST_PAYLOAD_12, TEST_PAYLOAD_2,
-            TEST_PAYLOAD_3, TEST_PAYLOAD_4, TEST_PAYLOAD_5, TEST_PAYLOAD_6, TEST_PAYLOAD_7,
-            TEST_PAYLOAD_8, TEST_PAYLOAD_9,
+            TEST_PAYLOAD_1, TEST_PAYLOAD_10, TEST_PAYLOAD_11, TEST_PAYLOAD_12, TEST_PAYLOAD_13,
+            TEST_PAYLOAD_2, TEST_PAYLOAD_3, TEST_PAYLOAD_4, TEST_PAYLOAD_5, TEST_PAYLOAD_6,
+            TEST_PAYLOAD_7, TEST_PAYLOAD_8, TEST_PAYLOAD_9,
         },
     };
 
@@ -1152,5 +1152,11 @@ mod tests {
     fn complex_payload_12() {
         init();
         TEST_PAYLOAD_12.parse_and_check_hash_matches_with_debug_smt();
+    }
+
+    #[test]
+    fn complex_payload_13() {
+        init();
+        TEST_PAYLOAD_13.parse_and_check_hash_matches_with_debug_smt();
     }
 }
