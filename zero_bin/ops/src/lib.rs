17535a910f051fa49926f6f830464715a34d0ffe
BGluth <gluthb@gmail.com>
2024-04-19 09:07:23 -0600

    Removed agg & block proof timing logic

--- zero_bin/ops/src/lib.rs
+++ zero_bin/ops/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ops::RangeInclusive, time::Instant};
+use std::time::Instant;
 
 use common::prover_state::p_state;
 use keccak_hash::keccak;
@@ -11,7 +11,7 @@ use proof_gen::{
     proof_types::{AggregatableProof, GeneratedAggProof, GeneratedBlockProof},
 };
 use serde::{Deserialize, Serialize};
-use trace_decoder::types::{BlockHeight, TxnProofGenIR};
+use trace_decoder::types::TxnProofGenIR;
 use tracing::{event, info_span, Level};
 
 registry!();
@@ -19,7 +19,7 @@ registry!();
 #[derive(Deserialize, Serialize, RemoteExecute)]
 pub struct TxProof;
 
-fn run_and_wrap_closure_in_elapsed_span<F, O>(f: F, ident: String) -> Result<O>
+fn run_and_wrap_txn_proof_in_elapsed_span<F, O>(f: F, ident: String) -> Result<O>
 where
     F: Fn() -> Result<O>,
 {
@@ -28,7 +28,12 @@ where
 
     let proof = f()?;
 
-    event!(Level::INFO, "Proof {:.4} took {:?}", ident, start.elapsed());
+    event!(
+        Level::INFO,
+        "txn proof {:.4} took {:?}",
+        ident,
+        start.elapsed()
+    );
     Ok(proof)
 }
 
@@ -40,7 +45,7 @@ impl Operation for TxProof {
     fn execute(&self, input: Self::Input) -> Result<Self::Output> {
         let txn_ident = Self::txn_ident(&input);
 
-        let proof = run_and_wrap_closure_in_elapsed_span(
+        let proof = run_and_wrap_txn_proof_in_elapsed_span(
             || {
                 common::prover_state::p_manager()
                     .generate_txn_proof(input.clone())
@@ -61,7 +66,7 @@ impl Operation for TxProof {
     fn execute(&self, input: Self::Input) -> Result<Self::Output> {
         let txn_ident = Self::txn_ident(&input);
 
-        run_and_wrap_closure_in_elapsed_span(
+        run_and_wrap_txn_proof_in_elapsed_span(
             || {
                 evm_arithmetization::prover::testing::simulate_execution::<proof_gen::types::Field>(
                     input.clone(),
@@ -84,7 +89,7 @@ impl TxProof {
             .unwrap_or_else(|| "Dummy".to_string());
 
         format!(
-            "Txn b{} - {} ({})",
+            "b{} - {} ({})",
             ir.block_metadata.block_number, ir.txn_number_before, txn_hash_str
         )
     }
@@ -97,11 +102,7 @@ impl Monoid for AggProof {
     type Elem = AggregatableProof;
 
     fn combine(&self, a: Self::Elem, b: Self::Elem) -> Result<Self::Elem> {
-        let ident = Self::agg_ident(&a, &b);
-        let result = run_and_wrap_closure_in_elapsed_span(
-            || generate_agg_proof(p_state(), &a, &b).map_err(|e| FatalError::from(e).into()),
-            ident,
-        )?;
+        let result = generate_agg_proof(p_state(), &a, &b).map_err(FatalError::from)?;
 
         Ok(result.into())
     }
@@ -109,21 +110,6 @@ impl Monoid for AggProof {
     fn empty(&self) -> Self::Elem {
         // Expect that empty blocks are padded.
         unimplemented!("empty agg proof")
-    }
-}
-
-impl AggProof {
-    fn agg_ident(a: &AggregatableProof, b: &AggregatableProof) -> String {
-        let b_height = b_height_from_aggregatable_proof(a);
-        let a_range = proof_range_from_aggregatable_proof(a);
-        let b_range = proof_range_from_aggregatable_proof(b);
-
-        format!(
-            "Agg b{} - {}..={}",
-            b_height,
-            *a_range.start(),
-            *b_range.end()
-        )
     }
 }
 
@@ -137,50 +123,9 @@ impl Operation for BlockProof {
     type Output = GeneratedBlockProof;
 
     fn execute(&self, input: Self::Input) -> Result<Self::Output> {
-        let ident = Self::block_ident(&input);
-
-        run_and_wrap_closure_in_elapsed_span(
-            || {
-                generate_block_proof(p_state(), self.prev.as_ref(), &input)
-                    .map_err(|e| FatalError::from(e).into())
-            },
-            ident,
+        Ok(
+            generate_block_proof(p_state(), self.prev.as_ref(), &input)
+                .map_err(FatalError::from)?,
         )
-    }
-}
-
-impl BlockProof {
-    fn block_ident(p: &GeneratedAggProof) -> String {
-        let b_height = p.p_vals.block_metadata.block_number;
-        let b_range = aggregated_proof_range(p);
-
-        format!(
-            "Block b{} ({}..={})",
-            b_height,
-            *b_range.start(),
-            *b_range.end()
-        )
-    }
-}
-
-fn proof_range_from_aggregatable_proof(p: &AggregatableProof) -> RangeInclusive<usize> {
-    match p {
-        AggregatableProof::Txn(info) => {
-            let txn_idx = info.p_vals.extra_block_data.txn_number_before.as_usize();
-            txn_idx..=txn_idx
-        }
-        AggregatableProof::Agg(info) => aggregated_proof_range(info),
-    }
-}
-
-fn aggregated_proof_range(p: &GeneratedAggProof) -> RangeInclusive<usize> {
-    p.p_vals.extra_block_data.txn_number_before.as_usize()
-        ..=p.p_vals.extra_block_data.txn_number_after.as_usize()
-}
-
-fn b_height_from_aggregatable_proof(p: &AggregatableProof) -> BlockHeight {
-    match p {
-        AggregatableProof::Txn(info) => info.p_vals.block_metadata.block_number.as_u64(),
-        AggregatableProof::Agg(info) => info.p_vals.block_metadata.block_number.as_u64(),
     }
 }
