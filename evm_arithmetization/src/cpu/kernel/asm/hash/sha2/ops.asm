af8123501d6b28c362c1e458663ef004c3926603
Robin Salen <salenrobin@gmail.com>
2024-05-17 12:27:39 +0800

    Speed up rotr macro

--- evm_arithmetization/src/cpu/kernel/asm/hash/sha2/ops.asm
+++ evm_arithmetization/src/cpu/kernel/asm/hash/sha2/ops.asm
@@ -1,14 +1,14 @@
 // 32-bit right rotation
 %macro rotr(rot)
     // stack: value
+    DUP1
+    // stack: value, value
     PUSH $rot
-    // stack: rot, value
-    DUP2
-    DUP2
-    // stack: rot, value, rot, value
+    // stack: rot, value, value
     SHR
-    // stack: value >> rot, rot, value
-    %stack (shifted, rot, value) -> (rot, value, shifted)
+    // stack: value >> rot, value
+    SWAP1
+    PUSH $rot
     // stack: rot, value, value >> rot
     PUSH 32
     SUB
