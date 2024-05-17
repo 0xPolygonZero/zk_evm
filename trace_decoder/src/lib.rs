ca907acd1a05d828e528ffb1035ecf9ca83a83db
BGluth <gluthb@gmail.com>
2024-05-16 20:15:57 -0600

    Re-added feature gating

--- trace_decoder/src/lib.rs
+++ trace_decoder/src/lib.rs
@@ -116,6 +116,12 @@
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
 
+#[cfg(not(any(feature = "mpt", feature = "smt")))]
+compile_error! {"Either the \"mpt\" or \"smt\" feature (but not both) must be enabled"}
+
+#[cfg(all(feature = "mpt", feature = "smt"))]
+compile_error! {"The features \"mpt\" and \"smt\" are mutually exclusive and can not be enabled at the same time"}
+
 mod aliased_crate_types;
 /// Provides debugging tools and a compact representation of state and storage
 /// tries, used in tests.
