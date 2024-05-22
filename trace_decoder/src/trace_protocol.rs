e66facfa539b601481ac31965c3195f4a798ff89
frisitano <francesco.risitano95@gmail.com>
2024-05-22 17:08:59 +0400

    support natvie tracing

--- trace_decoder/src/trace_protocol.rs
+++ trace_decoder/src/trace_protocol.rs
@@ -21,7 +21,7 @@
 //! sequentialize the entire proof generation process. So in the end, it's ideal
 //! if we can get this information sent to us instead.
 
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 
 use ethereum_types::{Address, U256};
 use mpt_trie::partial_trie::HashedPartialTrie;
