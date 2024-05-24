70037643787647012bd9132653f7dadf6dcc0e04
Robin Salen <30937548+Nashtare@users.noreply.github.com>
2024-05-24 09:51:43 -0400

    perf: Improve blake2 precompile (#239)
    * Speed-up some blake2 components
    
    * Some more optimizations
    
    * Cleanup
    
    * Endline
    
    * Some more speed-up
    
    * Fix comments

--- evm_arithmetization/src/cpu/kernel/asm/hash/blake2/ops.asm
+++ evm_arithmetization/src/cpu/kernel/asm/hash/blake2/ops.asm
@@ -1,14 +1,14 @@
 // 64-bit right rotation
 %macro rotr_64(rot)
     // stack: value
+    DUP1
+    // stack: value, value
     PUSH $rot
-    // stack: rot, value
-    DUP2
-    DUP2
-    // stack: rot, value, rot, value
+    // stack: rot, value, value
     SHR
-    // stack: value >> rot, rot, value
-    %stack (shifted, rot, value) -> (rot, value, shifted)
+    // stack: value >> rot, value
+    SWAP1
+    PUSH $rot
     // stack: rot, value, value >> rot
     PUSH 64
     SUB
