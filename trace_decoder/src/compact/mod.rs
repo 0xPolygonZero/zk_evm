4b36fc2b3cffd396d5ab73144fc50fc7abcb27c6
BGluth <gluthb@gmail.com>
2024-05-24 10:13:34 -0600

    Split compact testing into multiple modules
    - Required because we can only run the tests when the appropiate testing
      flag is enabled.

--- trace_decoder/src/compact/mod.rs
+++ trace_decoder/src/compact/mod.rs
@@ -7,9 +7,15 @@ cfg_if! {
     if #[cfg(feature = "mpt")] {
         pub mod compact_mpt_processing;
         pub mod compact_to_mpt_trie;
+
+        #[cfg(test)]
+        mod complex_test_payloads_mpt;
     } else if #[cfg(feature = "smt")] {
         pub mod compact_smt_processing;
         pub mod compact_to_smt_trie;
+
+        #[cfg(test)]
+        mod complex_test_payloads_smt;
     }
 }
 
