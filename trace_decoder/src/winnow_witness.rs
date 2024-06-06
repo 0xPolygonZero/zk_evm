1500a89237e26480ccca4602e101ad07ea071ed5
0xaatif <asyed@polygon.technology>
2024-06-06 04:42:38 +0100

    feat: first complex test vector passes

--- trace_decoder/src/winnow_witness.rs
+++ trace_decoder/src/winnow_witness.rs
@@ -2,15 +2,14 @@
 //!
 //! Based on [this specification](*https://gist.github.com/mandrigin/ff7eccf30d0ef9c572bafcb0ab665cff#the-bytes-layout)
 
-use std::{any::type_name, io, mem::size_of};
+use std::{any::type_name, io};
 
 use ethereum_types::U256;
-use keccak_hash::H256;
-use mpt_trie_type_1::nibbles::Nibbles;
+use nunny::NonEmpty;
 use serde::de::DeserializeOwned;
 use winnow::{
-    combinator::{alt, empty, eof, preceded, repeat_till, trace},
-    error::{ErrMode, ErrorKind, FromExternalError, TreeError},
+    combinator::{empty, eof, repeat_till, trace},
+    error::{ErrMode, ErrorKind, FromExternalError},
     stream::Stream,
     token::{any, literal, take},
     PResult, Parser as _,
@@ -19,31 +18,48 @@ use winnow::{
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Witness {
     pub header: V1Header,
-    pub instructions: Vec<Instruction>,
+    pub instructions: NonEmpty<Vec<Instruction>>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct V1Header;
 
+/// Names are taken from the spec.
+/// Spec also requires sequences to be non-empty
+///
+/// CBOR supports unsigned integers up to `2^64 -1`[^1], so that's what we use
+/// for native integers.
+///
+/// [^1]: <https://en.wikipedia.org/wiki/CBOR#Integers_(types_0_and_1)>
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Instruction {
     Leaf {
-        key: Nibbles,
-        value: Vec<u8>,
+        /// `ENCODE_KEY`-ed
+        key: NonEmpty<Vec<u8>>,
+        value: NonEmpty<Vec<u8>>,
     },
-    Extension(Nibbles),
+    // BUG: the spec has `EXTENSION` and `ACCOUNT_LEAF` inconsistent
+    // between the instruction list and encoding list
+    Extension {
+        /// `ENCODE_KEY`-ed
+        key: NonEmpty<Vec<u8>>,
+    },
     Branch {
-        mask: u32,
+        mask: u64,
     },
-    Hash(H256),
-    Code(Vec<u8>),
+    Hash {
+        raw_hash: [u8; 32],
+    },
+    Code {
+        raw_code: NonEmpty<Vec<u8>>,
+    },
     AccountLeaf {
-        key: Nibbles,
-        nonce: Option<U256>,
+        /// `ENCODE_KEY`-ed
+        key: NonEmpty<Vec<u8>>,
+        nonce: Option<u64>,
+        // BUG: see decode site
         balance: Option<U256>,
-        // according to the spec, this should be `has_code: bool`,
-        // but in actuality, there's an additional field here.
-        code_size: Option<u64>,
+        has_code: bool,
         has_storage: bool,
     },
     NewTrie,
@@ -70,8 +86,8 @@ impl<'a, E> ParserError<'a> for E where
 fn witness<'a, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<Witness, E> {
     Ok(Witness {
         header: header.parse_next(input)?,
-        instructions: repeat_till(.., instruction, eof)
-            .map(|(it, _)| it)
+        instructions: repeat_till(1.., instruction, eof)
+            .map(|(it, _)| NonEmpty::<Vec<_>>::new(it).unwrap())
             .parse_next(input)?,
     })
 }
@@ -87,13 +103,19 @@ fn instruction<'a, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<Instruct
     match opcode {
         0x00 => trace(
             "leaf",
-            (key, cbor).map(|(key, value)| Instruction::Leaf { key, value }),
+            (cbor, cbor).map(|(key, value)| Instruction::Leaf { key, value }),
         )
         .parse_next(input),
-        0x01 => trace("extension", key.map(Instruction::Extension)).parse_next(input),
+        0x01 => {
+            trace("extension", cbor.map(|key| Instruction::Extension { key })).parse_next(input)
+        }
         0x02 => trace("branch", cbor.map(|mask| Instruction::Branch { mask })).parse_next(input),
-        0x03 => trace("hash", h256.map(Instruction::Hash)).parse_next(input),
-        0x04 => trace("code", cbor.map(Instruction::Code)).parse_next(input),
+        0x03 => {
+            trace("hash", array.map(|raw_hash| Instruction::Hash { raw_hash })).parse_next(input)
+        }
+        0x04 => {
+            trace("code", cbor.map(|raw_code| Instruction::Code { raw_code })).parse_next(input)
+        }
         0x05 => trace("account_leaf", account_leaf).parse_next(input),
         0xBB => trace("new_trie", empty.value(Instruction::NewTrie)).parse_next(input),
         _ => {
@@ -120,7 +142,7 @@ fn account_leaf<'a, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<Instruc
         }
     }
 
-    let key = key.parse_next(input)?;
+    let key = cbor.parse_next(input)?;
     let flags = any
         .try_map(|byte| AccountLeafFlags::from_bits(byte).ok_or(UnrecognisedBits))
         .parse_next(input)?;
@@ -128,23 +150,29 @@ fn account_leaf<'a, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<Instruc
     Ok(Instruction::AccountLeaf {
         key,
         nonce: match flags.contains(AccountLeafFlags::ENCODES_NONCE) {
-            true => Some(trace("nonce", u256).parse_next(input)?),
+            true => Some(trace("nonce", cbor).parse_next(input)?),
             false => None,
         },
         balance: match flags.contains(AccountLeafFlags::ENCODES_BALANCE) {
-            true => Some(trace("balance", u256).parse_next(input)?),
-            false => None,
-        },
-        // TODO(0xaatif): brendan's code deviates from the spec and reads a u64 if HAS_CODE
-        code_size: match flags.contains(AccountLeafFlags::HAS_CODE) {
-            true => Some((trace("code_size", cbor)).parse_next(input)?),
+            // BUG: the spec says CBOR(balance), where we'd expect CBOR integer,
+            //      but actually we read a cbor vec, and decode that as BE
+            true => Some(
+                trace(
+                    "balance",
+                    cbor::<Vec<u8>, _>.map(|bytes| U256::from_big_endian(&bytes)),
+                )
+                .parse_next(input)?,
+            ),
             false => None,
         },
         has_storage: flags.contains(AccountLeafFlags::HAS_STORAGE),
+        has_code: flags.contains(AccountLeafFlags::HAS_CODE),
     })
 }
 
-fn cbor<'a, T: DeserializeOwned, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<T, E> {
+fn cbor<'a, T: DeserializeOwned + std::fmt::Debug, E: ParserError<'a>>(
+    input: &mut &'a [u8],
+) -> PResult<T, E> {
     trace(
         format!("cbor{{{}}}", type_name::<T>()),
         |input: &mut &'a [u8]| {
@@ -163,79 +191,6 @@ fn cbor<'a, T: DeserializeOwned, E: ParserError<'a>>(input: &mut &'a [u8]) -> PR
         },
     )
     .parse_next(input)
-}
-
-fn key<'a, E>(input: &mut &'a [u8]) -> PResult<Nibbles, E>
-where
-    E: ParserError<'a> + FromExternalError<&'a [u8], ciborium::de::Error<io::Error>>,
-{
-    trace(
-        "key",
-        cbor::<Vec<u8>, _>.map(|bytes| {
-            let mut key = Nibbles::default();
-
-            if bytes.is_empty() {
-                return key;
-            }
-
-            // I have no idea why Erigon is doing this with their keys, as I'm don't think
-            // this is part of the yellow paper at all?
-            if bytes.len() == 1 {
-                let low_nib = bytes[0] & 0b00001111;
-                key.push_nibble_back(low_nib);
-            }
-
-            let flags = bytes[0];
-            let is_odd = (flags & 0b00000001) != 0;
-            let has_term = (flags & 0b00000010) != 0;
-
-            // ... Term bit seems to have no effect on the key?
-            let actual_key_bytes = match has_term {
-                false => &bytes[1..],
-                true => &bytes[1..],
-            };
-
-            if actual_key_bytes.is_empty() {
-                return key;
-            }
-
-            let final_byte_idx = actual_key_bytes.len() - 1;
-
-            // The compact key format is kind of weird. We need to read the nibbles
-            // backwards from how we expect it internally.
-            for byte in &actual_key_bytes[..(final_byte_idx)] {
-                let high_nib = (byte & 0b11110000) >> 4;
-                let low_nib = byte & 0b00001111;
-
-                key.push_nibble_back(high_nib);
-                key.push_nibble_back(low_nib);
-            }
-
-            // The final byte we might need to ignore the last nibble, so we need to do it
-            // separately.
-            let final_byte = actual_key_bytes[final_byte_idx];
-            let high_nib = (final_byte & 0b11110000) >> 4;
-            key.push_nibble_back(high_nib);
-
-            if !is_odd {
-                let low_nib = final_byte & 0b00001111;
-                key.push_nibble_back(low_nib);
-            }
-
-            key
-        }),
-    )
-    .parse_next(input)
-}
-
-fn h256<'a, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<H256, E> {
-    array.map(H256).parse_next(input)
-}
-
-fn u256<'a, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<U256, E> {
-    array::<{ size_of::<U256>() }, _>
-        .map(|it| U256::from_big_endian(&it))
-        .parse_next(input)
 }
 
 fn array<'a, const N: usize, E: ParserError<'a>>(input: &mut &'a [u8]) -> PResult<[u8; N], E> {
@@ -264,11 +219,22 @@ fn cbor_test_vectors() {
     do_test(b"\x17", 23, cbor);
 }
 
-#[test]
-fn witness_test_vectors() {
+#[cfg(test)]
+mod witness_test_vectors {
     use hex_literal::hex;
-    use winnow::error::TreeError;
+    type Error = winnow::error::ContextError;
 
-    let simple = hex!("01004110443132333400411044313233340218300042035044313233350218180158200000000000000000000000000000000000000000000000000000000000000012");
-    dbg!(witness::<TreeError<_>>.parse(&simple)).unwrap();
+    use super::*;
+
+    #[test]
+    fn simple() {
+        let src = hex!("01004110443132333400411044313233340218300042035044313233350218180158200000000000000000000000000000000000000000000000000000000000000012");
+        dbg!(witness::<Error>.parse(&src).unwrap());
+    }
+
+    #[test]
+    fn one() {
+        let src = hex!("01055821033601462093b5945d1676df093446790fd31b20e7b12a2e8e5e09d068109616b0084a021e19e0c9bab240000005582103468288056310c82aa4c01a7e12a10f8111a0560e72b700555479031b86c357d0084101031a697e814758281972fcd13bc9707dbcd2f195986b05463d7b78426508445a0405582103b70e80538acdabd6137353b0f9d8d149f4dba91e8be2e7946e409bfdbe685b900841010558210389802d6ed1a28b049e9d4fe5334c5902fd9bc00c42821c82f82ee2da10be90800841010558200256274a27dd7524955417c11ecd917251cc7c4c8310f4c7e4bd3c304d3d9a79084a021e19e0c9bab2400000055820023ab0970b73895b8c9959bae685c3a19f45eb5ad89d42b52a340ec4ac204d190841010219102005582103876da518a393dbd067dc72abfa08d475ed6447fca96d92ec3f9e7eba503ca6100841010558210352688a8f926c816ca1e079067caba944f158e764817b83fc43594370ca9cf62008410105582103690b239ba3aaf993e443ae14aeffc44cf8d9931a79baed9fa141d0e4506e131008410102196573");
+        dbg!(witness::<Error>.parse(&src).unwrap());
+    }
 }
