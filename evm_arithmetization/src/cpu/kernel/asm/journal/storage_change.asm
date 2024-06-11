287cddd46c578907fd46a95cf5781955cd0d0c75
Alonso Gonzalez <alonso.gon@gmail.com>
2024-06-11 23:58:15 +0200

    Merge with journaling branch

--- evm_arithmetization/src/cpu/kernel/asm/journal/storage_change.asm
+++ evm_arithmetization/src/cpu/kernel/asm/journal/storage_change.asm
@@ -4,29 +4,6 @@
     %journal_add_3(@JOURNAL_ENTRY_STORAGE_CHANGE)
 %endmacro
 
-global revert_storage_change_original:
-    // stack: entry_type, ptr, retdest
-    POP
-    %journal_load_3
-    // stack: address, slot, prev_value, retdest
-    DUP3 ISZERO %jumpi(delete)
-    // stack: address, slot, prev_value, retdest
-    SWAP1 %slot_to_storage_key
-    // stack: storage_key, address, prev_value, retdest
-    PUSH 64 // storage_key has 64 nibbles
-    // stack: 64, storage_key, address, prev_value, retdest
-    DUP3 %mpt_read_state_trie
-    DUP1 ISZERO %jumpi(panic)
-    // stack: account_ptr, 64, storage_key, address, prev_value, retdest
-    %add_const(2)
-    // stack: storage_root_ptr_ptr, 64, storage_key, address, prev_value, retdest
-    %mload_trie_data
-    %get_trie_data_size
-    DUP6 %append_to_trie_data
-    %stack (prev_value_ptr, storage_root_ptr, num_nibbles, storage_key, address, prev_value, retdest) ->
-        (storage_root_ptr, num_nibbles, storage_key, prev_value_ptr, new_storage_root, address, retdest)
-    %jump(mpt_insert)
-
 global revert_storage_change:
     // stack: entry_type, ptr, retdest
     POP
@@ -40,23 +17,6 @@ global revert_storage_change:
     // stack: value_ptr, prev_value, retdest
     %mstore_trie_data
     JUMP
-
-delete_original:
-    // stack: address, slot, prev_value, retdest
-    SWAP2 POP
-    %stack (slot, address, retdest) -> (slot, new_storage_root, address, retdest)
-    %slot_to_storage_key
-    // stack: storage_key, new_storage_root, address, retdest
-    PUSH 64 // storage_key has 64 nibbles
-    // stack: 64, storage_key, new_storage_root, address, retdest
-    DUP4 %mpt_read_state_trie
-    DUP1 ISZERO %jumpi(panic)
-    // stack: account_ptr, 64, storage_key, new_storage_root, address, retdest
-    %add_const(2)
-    // stack: storage_root_ptr_ptr, 64, storage_key, new_storage_root, address, retdest
-    %mload_trie_data
-    // stack: storage_root_ptr, 64, storage_key, new_storage_root, address, retdest
-    %jump(mpt_delete)
 
 delete:
     // stack: address, slot, prev_value, retdest
