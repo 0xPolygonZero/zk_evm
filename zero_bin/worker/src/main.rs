aa5fd54b89348c8a521fc775b0b4af355302c8b8
0xaatif <asyed@polygon.technology>
2024-06-19 12:27:48 +0100

    doc: debt

--- zero_bin/worker/src/main.rs
+++ zero_bin/worker/src/main.rs
@@ -7,9 +7,11 @@ use zero_bin_common::prover_state::cli::CliProverStateConfig;
 
 mod init;
 
+// TODO: https://github.com/0xPolygonZero/zk_evm/issues/302
+//       this should probably be removed.
 #[cfg(not(target_env = "msvc"))]
 #[global_allocator]
-static GLOBAL: Jemalloc = jemallocator::Jemalloc;
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[derive(Parser, Debug)]
 struct Cli {
