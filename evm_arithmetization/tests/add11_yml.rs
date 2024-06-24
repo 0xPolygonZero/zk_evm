f54864a2fcf7a8b4b7e1b6ff8fa14eab6fc1e202
Alonso Gonzalez <alonso.gon@gmail.com>
2024-06-24 17:54:20 +0200

    [WIP] fixing LoopCallsDepthThenRevert3_d0g0v0_Shanghai.json

--- evm_arithmetization/tests/add11_yml.rs
+++ evm_arithmetization/tests/add11_yml.rs
@@ -165,10 +165,9 @@ fn add11_yml() -> anyhow::Result<()> {
         },
     };
 
-    let bytes = std::fs::read(
-        "/Users/agonzalez/Downloads/TouchToEmptyAccountRevert3_Paris_d0g0v0_Shanghai.json",
-    )
-    .unwrap();
+    let bytes =
+        std::fs::read("/Users/agonzalez/Downloads/LoopCallsDepthThenRevert3_d0g0v0_Shanghai.json")
+            .unwrap();
     let inputs = serde_json::from_slice(&bytes).unwrap();
 
     let mut timing = TimingTree::new("prove", log::Level::Debug);
