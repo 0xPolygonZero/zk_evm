b992e9effaa816588c47eba1dc2297e8a1702a18
Robin Salen <salenrobin@gmail.com>
2024-07-16 09:11:52 -0400

    Remove leftover

--- zero_bin/prover/src/lib.rs
+++ zero_bin/prover/src/lib.rs
@@ -47,7 +47,6 @@ impl BlockProverInput {
         use paladin::directive::{Directive, IndexedStream};
 
         let block_number = self.get_block_number();
-        info!("Proving block {block_number}");
 
         let other_data = self.other_data;
         let txs = self.block_trace.into_txn_proof_gen_ir(
