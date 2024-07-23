098fe75761f6a1153c4e3c09fcceda57143fc9b3
Marko Atanasievski <matanasievski@polygon.technology>
2024-07-23 17:09:03 +0200

    fix: conficts

--- zero_bin/prover/src/cli.rs
+++ zero_bin/prover/src/cli.rs
@@ -1,19 +1,21 @@
 use clap::Args;
 
+const HELP_HEADING: &str = "Prover options";
+
 /// Represents the main configuration structure for the runtime.
 #[derive(Args, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Default)]
 pub struct CliProverConfig {
     /// The log of the max number of CPU cycles per proof.
-    #[arg(short, long, default_value_t = 20)]
+    #[arg(short, long, help_heading = HELP_HEADING, default_value_t = 20)]
     max_cpu_len_log: usize,
     /// Number of transactions in a batch to process at once
-    #[arg(short, long, default_value_t = 1)]
+    #[arg(short, long, help_heading = HELP_HEADING, default_value_t = 1)]
     batch_size: usize,
     /// Number of segments to keep in memory and prove in parallel
-    #[arg(long, default_value_t = 64)]
+    #[arg(short='c', long, help_heading = HELP_HEADING, default_value_t = 64)]
     segment_chunk_size: usize,
     /// If true, save the public inputs to disk on error.
-    #[arg(short, long, default_value_t = false)]
+    #[arg(short='i', long, help_heading = HELP_HEADING, default_value_t = false)]
     save_inputs_on_error: bool,
 }
 
