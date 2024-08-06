52ab1aa9bdc41cd0f870986621f62343b4916f18
Arpit Temani <temaniarpit27@gmail.com>
2024-08-06 21:27:01 +0530

    implement version command for all zero bin binaries (#451)
    * implement version command for all zero bin binaries
    
    * fix clippy issues
    
    * fix review comments
    
    * Update zero_bin/README.md
    
    Co-authored-by: Robin Salen <30937548+Nashtare@users.noreply.github.com>
    
    * review fixes
    
    * fix cliipy issues in develop
    
    * Revert "fix cliipy issues in develop"
    
    This reverts commit 4615cb5305aba903d39dea80d3f44fe0f782aed8.
    
    * added removed code
    
    ---------
    
    Co-authored-by: Robin Salen <30937548+Nashtare@users.noreply.github.com>

--- zero_bin/common/build.rs
+++ zero_bin/common/build.rs
@@ -1,5 +1,15 @@
 use anyhow::Context as _;
+use vergen::{BuildBuilder, Emitter, RustcBuilder};
+
 fn main() -> anyhow::Result<()> {
+    let build_timestamp = BuildBuilder::default().build_timestamp(true).build()?;
+    let rust_commit_hash = RustcBuilder::default().commit_hash(true).build()?;
+
+    Emitter::default()
+        .add_instructions(&build_timestamp)?
+        .add_instructions(&rust_commit_hash)?
+        .emit()?;
+
     let meta = cargo_metadata::MetadataCommand::new()
         .exec()
         .context("failed to probe cargo-metadata")?;
@@ -10,10 +20,11 @@ fn main() -> anyhow::Result<()> {
         .context("couldn't find evm_arithmetization package")?
         .version;
     println!(
-        "cargo::rustc-env=EVM_ARITHMETIZATION_PACKAGE_VERSION={}.{}.x",
+        "cargo::rustc-env=EVM_ARITHMETIZATION_PKG_VER={}.{}.x",
         // patch version change should not prompt circuits regeneration
         version.major,
         version.minor
     );
+
     Ok(())
 }
