002381bcea5ad88f6d92a86a301fe5d08e52c5b2
Einar Rasmussen <erasmussen@polygon.technology>
2024-08-17 17:03:45 +0200

    feat: compute transaction hash from generation inputs

--- evm_arithmetization/src/generation/mod.rs
+++ evm_arithmetization/src/generation/mod.rs
@@ -1,8 +1,13 @@
 use std::collections::HashMap;
 
-use anyhow::anyhow;
+use ::rlp as rlp_crate;
+use anyhow::{anyhow, bail, Context as _, Error};
 use ethereum_types::{Address, BigEndianHash, H256, U256};
 use log::log_enabled;
+use mpt::transaction_testing::{
+    AccessListTransactionRlp, BlobTransactionRlp, FeeMarketTransactionRlp,
+};
+use mpt::LegacyReceiptRlp;
 use mpt_trie::partial_trie::{HashedPartialTrie, PartialTrie};
 use plonky2::field::extension::Extendable;
 use plonky2::field::polynomial::PolynomialValues;
@@ -12,6 +17,7 @@ use plonky2::timed;
 use plonky2::util::timing::TimingTree;
 use serde::{Deserialize, Serialize};
 use starky::config::StarkConfig;
+use tiny_keccak::Hasher as _;
 use GlobalMetadata::{
     ReceiptTrieRootDigestAfter, ReceiptTrieRootDigestBefore, StateTrieRootDigestAfter,
     StateTrieRootDigestBefore, TransactionTrieRootDigestAfter, TransactionTrieRootDigestBefore,
@@ -26,6 +32,7 @@ use crate::generation::trie_extractor::{get_receipt_trie, get_state_trie, get_tx
 use crate::memory::segments::Segment;
 use crate::proof::{BlockHashes, BlockMetadata, ExtraBlockData, PublicValues, TrieRoots};
 use crate::util::{h2u, u256_to_usize};
+use crate::witness::errors::ProgramError;
 use crate::witness::memory::{MemoryAddress, MemoryChannel};
 
 pub mod mpt;
@@ -78,6 +85,35 @@ pub struct GenerationInputs {
     /// The hash of the current block, and a list of the 256 previous block
     /// hashes.
     pub block_hashes: BlockHashes,
+}
+
+pub fn tx_hash(signed_txn: &[u8]) -> anyhow::Result<H256> {
+    let txn_type = signed_txn.first().context("No transaction type")?;
+    const LEGACY_TXN_TYPE: u8 = 0;
+    let txn_content = if *txn_type == LEGACY_TXN_TYPE {
+        signed_txn
+    } else {
+        &signed_txn[1..]
+    };
+
+    let mut hasher = tiny_keccak::Keccak::v256();
+    hasher.update(txn_content);
+    let mut output = [0u8; 32];
+    hasher.finalize(&mut output);
+    let hash = H256::from_slice(output.as_slice());
+
+    Ok(hash)
+}
+
+impl GenerationInputs {
+    pub fn tx_hash(&self) -> anyhow::Result<H256> {
+        let signed_tx = self
+            .signed_txn
+            .as_ref()
+            .context("GenerationInputs contained no signed transaction.")?
+            .as_slice();
+        tx_hash(signed_tx)
+    }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize, Default)]
@@ -375,4 +411,47 @@ pub(crate) fn output_debug_tries<F: RichField>(state: &GenerationState<F>) -> an
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr as _;
+
+    use anyhow::Context;
+    use keccak_hash::H256;
+
+    use crate::generation::tx_hash;
+
+    #[test]
+    fn four_tx_hash() -> anyhow::Result<()> {
+        // Bytes from `curl`` with `eth_getRawTransactionByHash`.
+
+        // type 0
+        let legacy_txn: Vec<u8> = hex::decode("f902ed82019f850169b62d9a8305833c9477edae6a5f332605720688c7fda7476476e8f83f80b902840938b20b0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000b8cf3bc88f84a2723c12182e87b5c769a1b6f607000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee0000000000000000000000001ce4f7a715a979f9bbdb2336af0d6204f7c53380000000000000000000000000bcba53e786f120fe39a71051f6bf5b4c2c5104fa0000000000000000000000000000000000000000007ac2f089d0029563794f1b000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000050000000000000000000000000799e39644f207baf37185479e0c23d0e5ed11dcc0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001a0000000000000000000000000000000000000000000000000000000000000024000000000000000000000000000000000000000000000000000000000000000010000000000000000000000001ce4f7a715a979f9bbdb2336af0d6204f7c53380000000000000000000000000b8cf3bc88f84a2723c12182e87b5c769a1b6f607000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000007a250d5630b4cf539739df2c5dacb4c659f2488d000000000000000000000000000000000000000000000000000000000000000026a036b15a66a40fffb09d63dba6214d67a50ec5d04232aa14bb4c110e62303c130da02af7397b8bef4fb36fadcedaeb0874ecd53d26a06ad760e2f179bea0851c5d59").context("Decoding failed.")?;
+        let legacy_txn_hash_expected: H256 =
+            H256::from_str("0x081ed7d1da9faaecb12993dea3759c9ad23a5fd5c5600022683611283ac6c0be")?;
+        let legacy_txn_hash_actual: H256 = tx_hash(legacy_txn.as_slice())?;
+        assert_eq!(legacy_txn_hash_expected, legacy_txn_hash_actual);
+
+        // type 1
+        // let accesslist_txn: Vec<u8> = todo!();
+        // let accesslist_txn_hash_expected: H256 = todo()!;
+        // let accesslist_txn_hash_actual: H256 = tx_hash(accesslist_txn.as_slice());
+        // assert_eq!(accesslist_txn_hash_expected, accesslist_txn_hash_actual);
+
+        // type 2
+        let feemarket_txn: Vec<u8> = "02f8d20182af3d847735940084f7aeb0ce8307a120946f1cdbbb4d53d226cf4b917bf768b94acbab616880b864f5537ede000000000000000000000000767fe9edc9e0df98e07454847909b5e959d7ca0e00000000000000000000000008dc8ffc2db71ea07537d1328b3be0799b6043960000000000000000000000000000000000000000000000077d5aebff37f80000c080a095d29381f45785f07b88d0d62cec774249f33cb49b0745aaf122e07a6d0ac415a0425440d7ba9f23d55ef15d3ac94da0807f594103a537e289c0c407b727f74578".into();
+        let feemarket_txn_hash_expected: H256 =
+            H256::from_str("0x864ae98fa9584d40e02419b74e89ddd16f8d4de155fe3d75b184ef6a4e529ad2")?;
+        let feemarket_txn_hash_actual: H256 = tx_hash(feemarket_txn.as_slice())?;
+        assert_eq!(feemarket_txn_hash_expected, feemarket_txn_hash_actual);
+
+        // type 3
+        // let blob_txn: Vec<u8> = todo!();
+        // let blob_txn_hash_expected: H256 = todo!();
+        // let blob_txn_hash_actual: H256 = tx_hash(blob_txn.as_slice());
+        // assert_eq!(blob_txn_hash_expected, blob_txn_hash_actual);
+
+        Ok(())
+    }
 }
