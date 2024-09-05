a9602c87af62e989579a745d2a6bd78fc31799a9
0xaatif <asyed@polygon.technology>
2024-09-05 18:42:47 +0100

    test: different batch sizes

--- trace_decoder/tests/consistent-with-header.rs
+++ trace_decoder/tests/consistent-with-header.rs
@@ -13,66 +13,69 @@ use mpt_trie::partial_trie::PartialTrie as _;
 
 fn main() -> anyhow::Result<()> {
     let mut trials = vec![];
-    for Case {
-        name,
-        header,
-        trace,
-        other,
-    } in cases()?
-    {
-        trials.push(Trial::test(name, move || {
-            let gen_inputs = trace_decoder::entrypoint(trace.clone(), other.clone(), 3, false)
-                .map_err(|e| format!("{e:?}"))?; // get the full cause chain
-            check!(gen_inputs.len() >= 2);
-            check!(
-                Some(other.checkpoint_state_trie_root)
-                    == gen_inputs.first().map(|it| it.tries.state_trie.hash())
-            );
-            let pairs = || gen_inputs.iter().tuple_windows::<(_, _)>();
-            check!(
-                pairs().position(|(before, after)| {
-                    before.trie_roots_after.state_root != after.tries.state_trie.hash()
-                }) == None
-            );
-            check!(
-                pairs().position(|(before, after)| {
-                    before.trie_roots_after.receipts_root != after.tries.receipts_trie.hash()
-                }) == None
-            );
-            check!(
-                pairs().position(|(before, after)| {
-                    before.trie_roots_after.transactions_root
-                        != after.tries.transactions_trie.hash()
-                }) == None
-            );
-            check!(
-                gen_inputs
-                    .last()
-                    .map(|it| it.trie_roots_after.state_root.compat())
-                    == Some(header.state_root)
-            );
-            check!(
-                gen_inputs
-                    .iter()
-                    .position(|it| it.block_metadata.block_timestamp != header.timestamp.into())
-                    == None
-            );
-            check!(
-                gen_inputs
-                    .last()
-                    .map(|it| it.block_hashes.cur_hash.compat())
-                    == Some(header.hash)
-            );
-            check!(
-                gen_inputs.iter().position(|it| it
-                    .block_hashes
-                    .prev_hashes
-                    .last()
-                    .is_some_and(|it| *it != header.parent_hash.compat()))
-                    == None
-            );
-            Ok(())
-        }));
+
+    for batch_size in [1, 3] {
+        for Case {
+            name,
+            header,
+            trace,
+            other,
+        } in cases()?
+        {
+            trials.push(Trial::test(format!("{name}@{batch_size}"), move || {
+                let gen_inputs = trace_decoder::entrypoint(trace, other.clone(), batch_size, false)
+                    .map_err(|e| format!("{e:?}"))?; // get the full cause chain
+                check!(gen_inputs.len() >= 2);
+                check!(
+                    Some(other.checkpoint_state_trie_root)
+                        == gen_inputs.first().map(|it| it.tries.state_trie.hash())
+                );
+                let pairs = || gen_inputs.iter().tuple_windows::<(_, _)>();
+                check!(
+                    pairs().position(|(before, after)| {
+                        before.trie_roots_after.state_root != after.tries.state_trie.hash()
+                    }) == None
+                );
+                check!(
+                    pairs().position(|(before, after)| {
+                        before.trie_roots_after.receipts_root != after.tries.receipts_trie.hash()
+                    }) == None
+                );
+                check!(
+                    pairs().position(|(before, after)| {
+                        before.trie_roots_after.transactions_root
+                            != after.tries.transactions_trie.hash()
+                    }) == None
+                );
+                check!(
+                    gen_inputs
+                        .last()
+                        .map(|it| it.trie_roots_after.state_root.compat())
+                        == Some(header.state_root)
+                );
+                check!(
+                    gen_inputs
+                        .iter()
+                        .position(|it| it.block_metadata.block_timestamp != header.timestamp.into())
+                        == None
+                );
+                check!(
+                    gen_inputs
+                        .last()
+                        .map(|it| it.block_hashes.cur_hash.compat())
+                        == Some(header.hash)
+                );
+                check!(
+                    gen_inputs.iter().position(|it| it
+                        .block_hashes
+                        .prev_hashes
+                        .last()
+                        .is_some_and(|it| *it != header.parent_hash.compat()))
+                        == None
+                );
+                Ok(())
+            }));
+        }
     }
     libtest_mimic::run(&Arguments::from_args(), trials).exit()
 }
--- trace_decoder/tests/simulate-execution.rs
+++ trace_decoder/tests/simulate-execution.rs
@@ -11,23 +11,29 @@ use plonky2::field::goldilocks_field::GoldilocksField;
 
 fn main() -> anyhow::Result<()> {
     let mut trials = vec![];
-    for Case {
-        name,
-        header: _,
-        trace,
-        other,
-    } in cases()?
-    {
-        let gen_inputs = trace_decoder::entrypoint(trace, other, 3, false)
-            .context(format!("error in `trace_decoder` for {name}"))?;
-        for (ix, gi) in gen_inputs.into_iter().enumerate() {
-            trials.push(Trial::test(format!("{name}/{ix}"), move || {
-                evm_arithmetization::prover::testing::simulate_execution_all_segments::<
-                    GoldilocksField,
-                >(gi, 19)
-                .map_err(|e| format!("{e:?}"))?; // get the full error chain
-                Ok(())
-            }))
+    for batch_size in [1, 3] {
+        for Case {
+            name,
+            header: _,
+            trace,
+            other,
+        } in cases()?
+        {
+            let gen_inputs = trace_decoder::entrypoint(trace, other, batch_size, false).context(
+                format!("error in `trace_decoder` for {name} at batch size {batch_size}"),
+            )?;
+            for (ix, gi) in gen_inputs.into_iter().enumerate() {
+                trials.push(Trial::test(
+                    format!("{name}@{batch_size}/{ix}"),
+                    move || {
+                        evm_arithmetization::prover::testing::simulate_execution_all_segments::<
+                            GoldilocksField,
+                        >(gi, 19)
+                        .map_err(|e| format!("{e:?}"))?; // get the full error chain
+                        Ok(())
+                    },
+                ))
+            }
         }
     }
     libtest_mimic::run(&Arguments::from_args(), trials).exit()
