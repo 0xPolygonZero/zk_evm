e0e232eb748817f2e5b3b284d0488a38a4c22f43
Robin Salen <salenrobin@gmail.com>
2024-09-24 14:45:58 -0400

    Add extra check

--- zero/src/bin/verifier.rs
+++ zero/src/bin/verifier.rs
@@ -5,8 +5,9 @@ use std::fs::File;
 use anyhow::Result;
 use clap::Parser;
 use dotenvy::dotenv;
+use evm_arithmetization::fixed_recursive_verifier::extract_two_to_one_block_hash;
 use serde_json::Deserializer;
-use tracing::info;
+use tracing::{info, warn};
 use zero::proof_types::{AggregatableBlockProof, GeneratedAggBlockProof, GeneratedBlockProof};
 use zero::prover_state::persistence::set_circuit_cache_dir_env_if_not_set;
 
@@ -66,16 +67,28 @@ fn main() -> Result<()> {
         cli::Command::AggBlock => {
             let input_proofs: Vec<GeneratedAggBlockProof> = serde_path_to_error::deserialize(des)?;
 
-            if input_proofs.into_iter().all(|block_proof| {
+            if input_proofs.into_iter().all(|wrapped_proof| {
+                // Assert consistency of the sequence of Public Values.
+                //
+                // This is not needed for proof verification, but allows to *trust*
+                // that the public info being attached in the clear are actually what
+                // was used internally when generating this proof, for external use.
+                let pis_match = if extract_two_to_one_block_hash(&wrapped_proof.intern.public_inputs) != &wrapped_proof.p_vals.hash().elements {
+                    warn!("The sequence of Public Values attached to this proof does not match the public inputs hash.");
+                    false
+                } else {
+                    true
+                };
+
                 verifier
-                    .verify_block_aggreg(&block_proof.intern)
+                    .verify_block_aggreg(&wrapped_proof.intern)
                     .map_err(|e| {
                         info!(
                             "Aggregated block proof verification failed with error: {:?}",
                             e
                         );
                     })
-                    .is_ok()
+                    .is_ok() && pis_match
             }) {
                 info!("All proofs verified successfully!");
             };
