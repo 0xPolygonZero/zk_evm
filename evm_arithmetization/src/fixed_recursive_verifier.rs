447cfb93cdf3dbdbc8a7de170c97c2b50fa2f455
Sai Deng <deng.sai@gmail.com>
2024-10-01 15:40:17 -0700

    fix ci error with "cdk_erigon"

--- evm_arithmetization/src/fixed_recursive_verifier.rs
+++ evm_arithmetization/src/fixed_recursive_verifier.rs
@@ -2999,6 +2999,7 @@ where
 }
 
 #[cfg(test)]
+#[cfg(not(feature = "cdk_erigon"))]
 mod tests {
     use plonky2::field::goldilocks_field::GoldilocksField;
     use plonky2::plonk::config::PoseidonGoldilocksConfig;
