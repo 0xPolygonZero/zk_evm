bb8b7864033a03b64614c7dbd4001b51837f858b
Marko Atanasievski <matanasievski@polygon.technology>
2024-10-03 13:51:19 +0200

    fix: remove debugging settings

--- scripts/check_proving_time.sh
+++ scripts/check_proving_time.sh
@@ -9,12 +9,12 @@ MEASUREMENT=$2
 
 REGRESSION_PERCENT_THRESHOLD="${REGRESSION_PERCENT_THRESHOLD:-10}"
 
-echo "Measured proving time is {$MEASUREMENT}, expected time ${EXPECTED}"
+echo "Measured proving time is ${MEASUREMENT}, expected time ${EXPECTED}"
 
 if (( $MEASUREMENT > $EXPECTED )); then
 	REGRESSION_PERCENT=$(( (($MEASUREMENT-$EXPECTED)*100) / $EXPECTED ))
 	if (( $REGRESSION_PERCENT > $REGRESSION_PERCENT_THRESHOLD )); then
-		echo "Measured proving time has more than {$REGRESSION_PERCENT_THRESHOLD}% regression"
+		echo "Measured proving time has more than ${REGRESSION_PERCENT_THRESHOLD}% regression"
 		exit 1
 	fi
 fi
