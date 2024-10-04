75908c19cdc86c67ff36ada93aebf447c24ac493
Einar Rasmussen <erasmussen@polygon.technology>
2024-10-04 14:26:05 +0200

    fixes

--- zero/src/rpc/jumpdest.rs
+++ zero/src/rpc/jumpdest.rs
@@ -383,7 +383,7 @@ pub(crate) fn generate_jumpdest_table(
                     );
                     continue;
                 }
-                assert!(jumpdest_offset.unwrap() < 24576);
+                ensure!(jumpdest_offset.unwrap() < 24576);
                 jumpdest_table.insert(*code_hash, *ctx, jumpdest_offset.unwrap());
             }
             "EXTCODECOPY" | "EXTCODESIZE" => {
@@ -474,7 +474,7 @@ mod compat {
         gas: u64,
         gas_cost: u64,
         depth: u64,
-        #[serde(deserialize_with = "error")]
+        #[serde(default, deserialize_with = "error")]
         error: Option<String>,
         stack: Option<Vec<U256>>,
         return_data: Option<Bytes>,
