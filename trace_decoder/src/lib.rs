c6581aafef0e056a5e917962234ee09d151e6542
Robin Salen <30937548+Nashtare@users.noreply.github.com>
2024-11-08 04:07:01 +0900

    Move `World` trait and related into `evm_arithmetization` crate (#783)
    * Move world into evm_arithmetization
    
    * Rustdoc

--- trace_decoder/src/lib.rs
+++ trace_decoder/src/lib.rs
@@ -56,20 +56,17 @@
 /// we can continue to do the main work of "executing" the transactions.
 ///
 /// The core of this library is agnostic over the (combined)
-/// state and storage representation - see [`world::World`] for more.
+/// state and storage representation - see [`evm_arithmetization::world::World`]
+/// for more.
 const _DEVELOPER_DOCS: () = ();
 
 mod interface;
 
 pub use interface::*;
-use keccak_hash::H256;
-use smt_trie::code::hash_bytecode_h256;
 
-mod tries;
 mod type1;
 mod type2;
 mod wire;
-mod world;
 
 pub use core::{entrypoint, WireDisposition};
 
@@ -100,27 +97,6 @@ mod hex {
             None => T::from_hex(&*s),
         }
         .map_err(D::Error::custom)
-    }
-}
-
-/// Utility trait to leverage a specific hash function across Type1 and Type2
-/// zkEVM variants.
-pub(crate) trait Hasher {
-    fn hash(bytes: &[u8]) -> H256;
-}
-
-pub(crate) struct PoseidonHash;
-pub(crate) struct KeccakHash;
-
-impl Hasher for PoseidonHash {
-    fn hash(bytes: &[u8]) -> H256 {
-        hash_bytecode_h256(bytes)
-    }
-}
-
-impl Hasher for KeccakHash {
-    fn hash(bytes: &[u8]) -> H256 {
-        keccak_hash::keccak(bytes)
     }
 }
 
