b9a0f9b1e16187825089b9e9127e0a0c472f9ec4
Einar Rasmussen <erasmussen@polygon.technology>
2024-11-12 20:27:19 +0100

    fix stats and padding

--- scripts/test_new_chain.sh
+++ scripts/test_new_chain.sh
@@ -21,9 +21,22 @@ RESULTS="witnesses/jerigon_new_chain.txt"
 RESULT_LEN=$(cat $RESULTS | wc -l)
 BLOCKS_TESTED=0
 
+function legend() {
+  printf "\n\nTable of exit codes\n" | tee -a $RESULTS
+  echo     "------------------------------------------------------------------------------------------"    | tee -a $RESULTS
+  echo "0 is success" | tee -a $RESULTS
+  echo "1 [unexpected] is other errors" | tee -a $RESULTS
+  echo "2 [unexpected] is undecided" | tee -a $RESULTS
+  echo "4 [expected] is Attempted to collapse an extension node" | tee -a $RESULTS
+  echo "5 [unexpected] is non-matching jumpdest tables" | tee -a $RESULTS
+  echo "6 [expected] is empty witness. Possibly due to Error: Failed to get proof for account" | tee -a $RESULTS
+  echo "7 [expected] is Found a Hash node during an insert in a PartialTrie" | tee -a $RESULTS
+  echo "8 [expected] is Attempted to delete a value that ended up inside a hash node" | tee -a $RESULTS
+  echo "9 [expected] is Memory allocation failed.  Increase RAM" | tee -a $RESULTS
+  echo "124 [undecided] is timeout.  Try increasing the proving timeout." | tee -a $RESULTS
+}
 
-function statistics()
-{
+function statistics() {
   PREFIX_LEN=$BLOCKS_TESTED
   wc -l $RESULTS
   cat $RESULTS | tail -n $PREFIX_LEN
@@ -32,17 +45,19 @@ function statistics()
   SUMFAIL=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f9  |  paste -s -d+ - | bc)
   SUMTOTAL=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f10 |  paste -s -d+ - | bc)
 
-  ZEROES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "0")
-  ONES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "1")
-  TWOS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "2")
-  THREES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "3")
-  FOURS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "4")
-  FIVES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "5")
-  SIXES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "6")
-  SEVENS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "7")
-  EIGHTS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "8")
-  NINES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "9")
-  TIMEOUTS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "124")
+  ZEROES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "0$")
+  ONES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "1$")
+  TWOS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "2$")
+  THREES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "3$")
+  FOURS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "4$")
+  FIVES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "5$")
+  SIXES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "6$")
+  SEVENS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "7$")
+  EIGHTS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "8$")
+  NINES=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "9$")
+  TIMEOUTS=$(cat $RESULTS | tail -n $PREFIX_LEN | tr -s ' ' | cut -d' ' -f4 | grep --count "124$")
+
+  legend
 
   printf "\n\nStatistics\n" | tee -a $RESULTS
   echo   "---------------------------------------------------------------------------------------"    | tee -a $RESULTS
@@ -393,23 +408,10 @@ BLOCKS="$(seq 6555)"
 echo "Testing:  $BLOCKS"
 
 
-
 printf "\n\nReport started: $(date)" | tee -a $RESULTS
-printf "\n\nTable of exit codes\n" | tee -a $RESULTS
-echo   "---------------------------------------------------------------------------------------"    | tee -a $RESULTS
-echo "0 is success" | tee -a $RESULTS
-echo "1 [unexpected] is other errors" | tee -a $RESULTS
-echo "2 [unexpected] is undecided" | tee -a $RESULTS
-echo "4 [expected] is Attempted to collapse an extension node" | tee -a $RESULTS
-echo "5 [unexpected] is non-matching jumpdest tables" | tee -a $RESULTS
-echo "6 [expected] is empty witness. Possibly due to Error: Failed to get proof for account" | tee -a $RESULTS
-echo "7 [expected] is Found a Hash node during an insert in a PartialTrie" | tee -a $RESULTS
-echo "8 [expected] is Attempted to delete a value that ended up inside a hash node" | tee -a $RESULTS
-echo "9 [expected] is Memory allocation failed.  Increase RAM" | tee -a $RESULTS
-echo "124 [undecided] is timeout.  Try increasing the proving timeout." | tee -a $RESULTS
-
-printf "\ngithash       block verdict   r  rpc-time  test-time total-time  tx-ok tx-none tx-total \n" | tee -a $RESULTS
-echo   "---------------------------------------------------------------------------------------"    | tee -a $RESULTS
+legend
+printf "\ngithash       block verdict   r   rpc-time   test-time  total-time  tx-ok tx-none tx-total\n" | tee -a $RESULTS
+echo     "------------------------------------------------------------------------------------------"    | tee -a $RESULTS
 
 for BLOCK in $BLOCKS; do
   TOTALTIME=0
@@ -420,7 +422,7 @@ for BLOCK in $BLOCKS; do
   SECONDS=0
   nice -19 -- "${REPO_ROOT}/target/release/rpc" --backoff 3000 --max-retries 100 --rpc-url $RPC --rpc-type jerigon --jumpdest-src client-fetched-structlogs --timeout 600 fetch --start-block $BLOCK --end-block $BLOCK 1> $WITNESS
   TOTALTIME=`echo -n $(($TOTALTIME + $SECONDS))`
-  DURATION_RPC=`date -u -d @"$SECONDS" +'%-Hh%-Mm%-Ss'`
+  DURATION_RPC=`date -u -d @"$SECONDS" +'%0Hh%0Mm%0Ss'`
   TXALL=`grep '"jumpdest_table":' $WITNESS | wc -l`
   TXNONE=`grep '"jumpdest_table": null' $WITNESS | wc -l`
   TXOK=`echo -n $(($TXALL - $TXNONE))`
@@ -430,8 +432,8 @@ for BLOCK in $BLOCKS; do
   timeout 5h nice -19 -- ./prove_stdio.sh $WITNESS test_only $BLOCK
   EXITCODE=$?
   TOTALTIME=`echo -n $(($TOTALTIME + $SECONDS))`
-  DURATION_PRV=`date -u -d @"$SECONDS" +'%-Hh%-Mm%-Ss'`
-  TOTALTIME=`date -u -d @"$TOTALTIME" +'%-Hh%-Mm%-Ss'`
+  DURATION_PRV=`date -u -d @"$SECONDS" +'%0Hh%0Mm%0Ss'`
+  TOTALTIME=`date -u -d @"$TOTALTIME" +'%0Hh%0Mm%0Ss'`
   if [ $EXITCODE -eq 0 ]
   then
     VERDICT="success"
